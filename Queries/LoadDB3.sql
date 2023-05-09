1648f1a2d7c65e803bbe0324e9e6b4c8c6cde938
nbarnett19 <127861184+nbarnett19@users.noreply.github.com>
2023-05-09 16:31:59 +0200

    Query Optimization
    Also toom screenshots of stats and execution plan

--- Queries/LoadDB3.sql
+++ Queries/LoadDB3.sql
@@ -1,6 +1,6 @@
 SET GLOBAL local_infile = true;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/books-upload.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/books-upload.csv'
 INTO TABLE Books
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
@@ -8,42 +8,42 @@ IGNORE 1 LINES;
 
 SELECT * FROM Books;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/BookCrossingDatasets/BX-Users-NEW3.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/BX-Users-NEW3.csv'
 INTO TABLE bc_users
 FIELDS TERMINATED BY ';'
 IGNORE 1 LINES;
 
 SELECT * FROM bc_users;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/BookCrossingDatasets/BX-Book-Ratings-NEW.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/BX-Book-Ratings-NEW.csv'
 INTO TABLE BC_User_Ratings
 FIELDS TERMINATED BY ';'
 IGNORE 1 LINES;
 
 SELECT * FROM BC_User_Ratings;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/awards_ids.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/awards_ids.csv'
 INTO TABLE awards
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
 SELECT * FROM awards;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/genres_ids.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/genres_ids.csv'
 INTO TABLE genres
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
 SELECT * FROM genres;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/book_genres_final.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/book_genres_final.csv'
 INTO TABLE book_genres
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
 
 SELECT * FROM book_genres;
 
-LOAD DATA LOCAL INFILE '/Users/Christina/OneDrive - Hochschule Luzern/DBM Project/Data/book_awards_final.csv'
+LOAD DATA LOCAL INFILE '/Users/natal/OneDrive/Documents/GitHub/book_recommendations/Datasets/book_awards_final.csv'
 INTO TABLE book_awards
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
---
+++ Query Optimization/Query2.sql
@@ -0,0 +1,122 @@
+#Optimized Query 2
+#Reordered columns and joins
+#Reduced group by
+#rordered filtering statements
+SELECT 
+	bg.GenreID AS GenreID
+	,gen.Name AS Genre
+	,us.UserID As UserID
+    ,us.Country AS Country_user
+	,us.Age AS User_age
+    ,rat.RATING AS Ratings 
+	,COUNT(ba.BookID) as Number_of_awards
+	,book.BookID AS BookID 
+	,book.Title AS Title
+	,book.Author AS Author
+	,book.Rating_GR AS Rating_GR 
+	,book.Language AS Book_language
+	,book.BookFormat As Book_format
+	,book.Pages AS Number_pages
+	,book.Publisher AS Publisher 
+From bookdb.books book
+INNER JOIN bookdb.book_genres bg ON book.BookID = bg.BookID
+INNER JOIN bookdb.genres gen ON bg.GenreID = gen.GenreID
+INNER JOIN bookdb.bc_user_ratings rat ON book.BookID = rat.BookID
+LEFT JOIN bookdb.bc_users us ON rat.UserID = us.UserID
+LEFT JOIN bookdb.book_awards ba on book.BookID = ba.BookID
+LEFT JOIN bookdb.awards awards on ba.AwardID = awards.AwardID
+WHERE us.Age <> 0
+AND rat.RATING <> 0
+AND bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17,15,7,19,28,5,12)
+Group by 
+	bg.GenreID
+	,gen.Name
+	,us.UserID
+    ,us.Country
+	,us.Age
+    ,rat.RATING
+	,book.BookID;
+
+#Query 3
+#added a subquery for awards instead of join
+SELECT
+	bg.GenreID AS GenreID
+	,gen.Name AS Genre
+	,us.UserID As UserID
+	,us.Country AS Country_user
+	,us.Age AS User_age
+	,rat.RATING AS Ratings
+	,(SELECT COUNT(*) FROM bookdb.book_awards ba WHERE book.BookID = ba.BookID) as Number_of_awards
+	,book.BookID AS BookID
+	,book.Title AS Title
+	,book.Author AS Author
+	,book.Rating_GR AS Rating_GR
+	,book.Language AS Book_language
+	,book.BookFormat As Book_format
+	,book.Pages AS Number_pages
+	,book.Publisher AS Publisher
+FROM bookdb.books book
+INNER JOIN bookdb.book_genres bg ON book.BookID = bg.BookID
+INNER JOIN bookdb.genres gen ON bg.GenreID = gen.GenreID
+INNER JOIN bookdb.bc_user_ratings rat ON book.BookID = rat.BookID
+LEFT JOIN bookdb.bc_users us ON rat.UserID = us.UserID
+WHERE us.Age <> 0
+AND rat.RATING <> 0
+AND bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17,15,7,19,28,5,12)
+GROUP BY
+	bg.GenreID
+	,gen.Name
+	,us.UserID
+	,us.Country
+	,us.Age
+	,rat.RATING
+	,book.BookID;
+    
+#Query 4
+#added indexes to speed up query
+
+CREATE INDEX idx_books_bookid ON bookdb.books(BookID);
+#DROP INDEX idx_books_bookid ON bookdb.books;
+CREATE INDEX idx_book_genres_bookid ON bookdb.book_genres(BookID);
+#DROP INDEX idx_book_genres_bookid ON bookdb.book_genres;
+CREATE INDEX idx_book_genres_genreid ON bookdb.book_genres(GenreID);
+#DROP INDEX idx_book_genres_genreid ON bookdb.book_genres;
+CREATE INDEX idx_genres_genreid ON bookdb.genres(GenreID);
+#DROP INDEX idx_genres_genreid ON bookdb.genres;
+CREATE INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings(BookID);
+#DROP INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings;
+CREATE INDEX idx_bc_users_userid ON bookdb.bc_users(UserID);
+#DROP INDEX idx_bc_users_userid ON bookdb.bc_users;
+
+SELECT
+	bg.GenreID AS GenreID
+	,gen.Name AS Genre
+	,us.UserID As UserID
+	,us.Country AS Country_user
+	,us.Age AS User_age
+	,rat.RATING AS Ratings
+	,(SELECT COUNT(*) FROM bookdb.book_awards ba WHERE book.BookID = ba.BookID) as Number_of_awards
+	,book.BookID AS BookID
+	,book.Title AS Title
+	,book.Author AS Author
+	,book.Rating_GR AS Rating_GR
+	,book.Language AS Book_language
+	,book.BookFormat As Book_format
+	,book.Pages AS Number_pages
+	,book.Publisher AS Publisher
+FROM bookdb.books book
+INNER JOIN bookdb.book_genres bg ON book.BookID = bg.BookID
+INNER JOIN bookdb.genres gen ON bg.GenreID = gen.GenreID
+INNER JOIN bookdb.bc_user_ratings rat ON book.BookID = rat.BookID
+LEFT JOIN bookdb.bc_users us ON rat.UserID = us.UserID
+WHERE us.Age <> 0
+AND rat.RATING <> 0
+AND bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17,15,7,19,28,5,12)
+GROUP BY
+	bg.GenreID
+	,gen.Name
+	,us.UserID
+	,us.Country
+	,us.Age
+	,rat.RATING
+	,book.BookID;
