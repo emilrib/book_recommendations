e0e39f57d3f7f6846c916365d652581b1d395ec2
emilrib <127395067+emilrib@users.noreply.github.com>
2023-06-08 18:22:42 +0200

    Add files via upload

--- Queries/Final_query.sql
+++ Queries/Final_query.sql
@@ -1,6 +1,50 @@
 #Final Query
+
+#unoptimized version
+
+SELECT 
+bg.GenreID AS GenreID
+,gen.Name AS Genre
+,us.UserID As UserID
+,us.Age AS User_age
+,rat.RATING AS Ratings 
+,book.BookFormat As Book_format
+,book.BookID AS BookID 
+,book.Title AS Title
+,book.Author AS Author
+,book.Publisher AS Publisher 
+,COUNT(ba.BookID) as Number_of_awards
+From bookdb.books book
+INNER JOIN bookdb.book_genres bg 
+ON book.BookID = bg.BookID
+INNER JOIN bookdb.genres gen 
+ON bg.GenreID = gen.GenreID
+INNER JOIN bookdb.bc_user_ratings rat 
+ON book.BookID = rat.BookID
+LEFT JOIN bookdb.bc_users us
+ON rat.UserID = us.UserID
+LEFT JOIN bookdb.book_awards ba
+on book.BookID = ba.BookID
+LEFT JOIN bookdb.awards awards
+on ba.AwardID = awards.AwardID
+WHERE bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17,15,7,19,28,5,12)
+and rat.RATING <> 0
+AND us.Age <> 0
+Group by 
+bg.GenreID 
+,gen.Name 
+,us.UserID 
+,us.Age 
+,rat.RATING 
+,book.BookFormat 
+,book.BookID  
+,book.Title 
+,book.Author 
+,book.Publisher;    
     
-#Query 4
+    
+
+#optimized version
 #added indexes to speed up query
 
 CREATE INDEX idx_books_bookid ON bookdb.books(BookID);
