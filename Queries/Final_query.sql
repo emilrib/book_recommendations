655c9b53702da101b18ac983be78da5eb25b849c
nbarnett19 <127861184+nbarnett19@users.noreply.github.com>
2023-06-11 05:55:04 +0200

    Updates

--- Queries/Final_query.sql
+++ Queries/Final_query.sql
@@ -1,5 +1,19 @@
-#Final Query
+#calculate the top 16 genres    
+SELECT
+  genr.GenreID
+  ,ge.Name
+  ,COUNT(genr.GenreID) AS `value_occurrence` 
+FROM bookdb.book_genres genr
+left join bookdb.genres ge
+on genr.GenreID = ge.GenreID
+GROUP BY 
+  genr.GenreID
+  ,ge.Name
+ORDER BY 
+ `value_occurrence` DESC
+LIMIT 18;
 
+#Final Query
 #unoptimized version
 
 SELECT 
@@ -41,24 +55,15 @@ bg.GenreID
 ,book.Title 
 ,book.Author 
 ,book.Publisher;    
-    
-    
+        
 
 #optimized version
 #added indexes to speed up query
 
 CREATE INDEX idx_books_bookid ON bookdb.books(BookID);
-#DROP INDEX idx_books_bookid ON bookdb.books;
 CREATE INDEX idx_book_genres_bookid ON bookdb.book_genres(BookID);
-#DROP INDEX idx_book_genres_bookid ON bookdb.book_genres;
-CREATE INDEX idx_book_genres_genreid ON bookdb.book_genres(GenreID);
-#DROP INDEX idx_book_genres_genreid ON bookdb.book_genres;
 CREATE INDEX idx_genres_genreid ON bookdb.genres(GenreID);
-#DROP INDEX idx_genres_genreid ON bookdb.genres;
-CREATE INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings(BookID);
-#DROP INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings;
 CREATE INDEX idx_bc_users_userid ON bookdb.bc_users(UserID);
-#DROP INDEX idx_bc_users_userid ON bookdb.bc_users;
 
 SELECT
     bg.GenreID AS GenreID
@@ -83,31 +88,15 @@ WHERE
     AND rat.RATING <> 0
     AND bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17, 15, 7, 19, 28, 5, 12)
 GROUP BY
-    bg.GenreID 
-    ,gen.Name 
-    ,us.UserID 
-    ,us.Age 
-    ,rat.RATING 
-    ,book.BookID
-    ,book.Title 
-    ,book.Author 
-    ,book.BookFormat
-    ,book.Publisher;
+	bg.GenreID
+	,gen.Name
+	,us.UserID
+	,us.Country
+	,us.Age
+	,rat.RATING
+	,book.BookID;
     
 
-#calculate the top 16 genres    
-SELECT
-  genr.GenreID
-  ,ge.Name
-  ,COUNT(genr.GenreID) AS `value_occurrence` 
-FROM bookdb.book_genres genr
-left join bookdb.genres ge
-on genr.GenreID = ge.GenreID
-GROUP BY 
-  genr.GenreID
-  ,ge.Name
-ORDER BY 
- `value_occurrence` DESC
-LIMIT 18;
+
     
     
--- Query Optimization/Query2.sql
+++ Query Optimization/Query2.sql
@@ -1,7 +1,7 @@
 #Optimized Query 2
 #Reordered columns and joins
 #Reduced group by
-#reordered filtering statements
+#rordered filtering statements
 SELECT 
 	bg.GenreID AS GenreID
 	,gen.Name AS Genre
@@ -79,12 +79,8 @@ CREATE INDEX idx_books_bookid ON bookdb.books(BookID);
 #DROP INDEX idx_books_bookid ON bookdb.books;
 CREATE INDEX idx_book_genres_bookid ON bookdb.book_genres(BookID);
 #DROP INDEX idx_book_genres_bookid ON bookdb.book_genres;
-CREATE INDEX idx_book_genres_genreid ON bookdb.book_genres(GenreID);
-#DROP INDEX idx_book_genres_genreid ON bookdb.book_genres;
 CREATE INDEX idx_genres_genreid ON bookdb.genres(GenreID);
 #DROP INDEX idx_genres_genreid ON bookdb.genres;
-CREATE INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings(BookID);
-#DROP INDEX idx_bc_user_ratings_bookid ON bookdb.bc_user_ratings;
 CREATE INDEX idx_bc_users_userid ON bookdb.bc_users(UserID);
 #DROP INDEX idx_bc_users_userid ON bookdb.bc_users;
 
@@ -120,50 +116,3 @@ GROUP BY
 	,us.Age
 	,rat.RATING
 	,book.BookID;
-	
-	
-#new query 
-
-WITH rating_count AS (
-    SELECT BookID
-    ,COUNT(Rating) AS Rating_Count
-    FROM bookdb.bc_user_ratings
-    GROUP BY BookID
-	)
-SELECT
-    bg.GenreID AS GenreID
-    ,gen.Name AS Genre
-    ,us.UserID AS UserID
-    ,us.Country AS Country_user
-    ,us.Age AS User_age
-    ,rat.RATING AS Ratings
-    ,rc.Rating_Count AS Number_of_ratings
-    ,(SELECT COUNT(*) FROM bookdb.book_awards ba WHERE book.BookID = ba.BookID) AS Number_of_awards
-    ,book.BookID AS BookID
-    ,book.Title AS Title
-    ,book.Author AS Author
-    ,book.Rating_GR AS Rating_GR
-    ,book.Language AS Book_language
-    ,book.BookFormat AS Book_format
-    ,book.Pages AS Number_pages
-    ,book.Publisher AS Publisher
-FROM
-    bookdb.books book
-    INNER JOIN bookdb.book_genres bg ON book.BookID = bg.BookID
-    INNER JOIN bookdb.genres gen ON bg.GenreID = gen.GenreID
-    LEFT JOIN rating_count rc ON book.BookID = rc.BookID
-    LEFT JOIN bookdb.bc_user_ratings rat ON book.BookID = rat.BookID
-    LEFT JOIN bookdb.bc_users us ON rat.UserID = us.UserID
-WHERE
-    us.Age <> 0
-    AND rat.RATING <> 0
-    AND bg.GenreID IN (2, 6, 4, 1, 42, 114, 26, 20, 21, 56, 17, 15, 7, 19, 28, 5, 12)
-GROUP BY
-    bg.GenreID
-    ,gen.Name
-    ,us.UserID
-    ,us.Country
-    ,us.Age
-    ,rat.RATING
-    ,rc.Rating_Count
-    ,book.BookID;
